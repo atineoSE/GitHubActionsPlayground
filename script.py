299fd43ea69fdab59209f468543abdd53396c118
Adrian Tineo <adrian@adriantineo.com>
2023-04-06 12:06:47 +0200

    Increment counter on every run

--- script.py
+++ script.py
@@ -2,7 +2,7 @@
 
 import os
 
-def main():
+def check_environment_vars():
     dummy_var = os.environ.get('DUMMY_VAR')
     dummy_secret = os.environ.get('DUMMY_SECRET')
     if dummy_var == "my_dummy_var":
@@ -12,8 +12,15 @@ def main():
     if dummy_secret == "my_dummy_secret":
     	print("Dummy secret retrieved successfully")
     else:
-    	print("Dummy secret not recognized")
+
+def increment_counter():
+    with open('counter', 'r+') as f: 
+        num = int(f.read()) 
+        num += 1 
+        f.seek(0) 
+        f.write(str(num))    
 
 if __name__ == '__main__':
-    main()
+    check_environment_vars()
+    increment_counter()
 
